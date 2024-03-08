5641e43743c784be182a498e717942b9b1c0eaa6
AndresZapataPascual <andres.zapata019@pascualbravo.edu.co>
2024-03-08 10:50:51 -0500

    login
    1

--- APP.js
+++ APP.js
@@ -1,1 +1,9 @@
+function crearCuenta(url) {
+    // Aquí puedes escribir el código que se ejecutará cuando se haga clic en el botón "Crear cuenta"
+    window.open(url, '_blank');
+}
 
+function iniciarSesion() {
+    // Aquí puedes escribir el código que se ejecutará cuando se haga clic en el botón "Iniciar Sesión"
+    console.log("Se hizo clic en el botón 'Iniciar Sesión'");
+}
